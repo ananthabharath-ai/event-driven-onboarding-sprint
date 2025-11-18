1e3fa41409a85bf251f3b26facd6f1674106cec1
AnanthaBharath <anantha.bharath@acuverconsulting.com>
2025-11-18 12:47:38 +0530

    Changed the dynamodbtestconfig to connect to the correct port

--- ProfileService/ProfileService/src/test/java/com/profile/config/DynamoDbTestContainerConfig.java
+++ ProfileService/ProfileService/src/test/java/com/profile/config/DynamoDbTestContainerConfig.java
@@ -1,13 +1,13 @@
 package com.profile.config;
 
-import java.net.URI;
-
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -15,48 +15,51 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
-/**
- * This class configures the DynamoDB-local container for our integration tests.
- */
-@TestConfiguration
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Testcontainers
+@Configuration
+@Profile("test") 
 public class DynamoDbTestContainerConfig {
 
-    // Note: The artifact is 'dynamodb' but the image is 'dynamodb-local'
-    // This uses the official Amazon image.
-    @Container
-    private static final GenericContainer<?> dynamodb = 
-        new GenericContainer<>("amazon/dynamodb-local:latest")
-            .withExposedPorts(8000)
-            .withCommand("-jar DynamoDBLocal.jar -inMemory -sharedDb");
+    private static final String DYNAMODB_IMAGE = "amazon/dynamodb-local:latest";
+    private static final int DYNAMODB_PORT = 8000;
 
+    // The DynamoDB container, managed by Testcontainers
+    @Container
+    public static final GenericContainer<?> dynamodb = new GenericContainer<>(DockerImageName.parse(DYNAMODB_IMAGE))
+            .withExposedPorts(DYNAMODB_PORT)
+            .withCommand("-jar", "DynamoDBLocal.jar", "-sharedDb", "-inMemory");
+
+    // Start the container
     static {
         dynamodb.start();
     }
 
     /**
-     * Creates a DynamoDbClient bean that points to the running container.
-     * @Primary ensures this bean overrides the default one.
+     * Creates the DynamoDbClient using the dynamically mapped host and port 
+     * from the running Testcontainer.
+     * This fixes the "Connection refused" error caused by hardcoded localhost connections.
      */
     @Bean
     @Primary
-    public DynamoDbClient dynamoDbClient() {
+    public DynamoDbClient dynamoDbClient() throws URISyntaxException {
+        // Dynamically get the host and port assigned by Docker/Testcontainers
+        String endpointUrl = String.format("http://%s:%d", dynamodb.getHost(), dynamodb.getMappedPort(DYNAMODB_PORT));
+
         return DynamoDbClient.builder()
-            .endpointOverride(URI.create("http://localhost:" + dynamodb.getMappedPort(8000)))
-            .credentialsProvider(StaticCredentialsProvider.create(
-                AwsBasicCredentials.create("test", "test")))
-            .region(Region.US_EAST_1)
-            .build();
+                .endpointOverride(new URI(endpointUrl))
+                .region(Region.US_EAST_1)
+                // Use dummy credentials for the local DynamoDB instance
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("fakeKey", "fakeSecret")))
+                .build();
     }
 
-    /**
-     * Creates the DynamoDbEnhancedClient bean that our repository needs.
-     */
     @Bean
-    @Primary
     public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
         return DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(dynamoDbClient)
-            .build();
+                .dynamoDbClient(dynamoDbClient)
+                .build();
     }
 }
