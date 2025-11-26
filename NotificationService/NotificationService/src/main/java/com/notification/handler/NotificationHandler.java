6e680dc2561755d3a9ef86813f13e22b965c920d
AnanthaBharath <anantha.bharath@acuverconsulting.com>
2025-11-26 14:54:40 +0530

    Changed the notification handler to handle null events

--- NotificationService/NotificationService/src/main/java/com/notification/handler/NotificationHandler.java
+++ NotificationService/NotificationService/src/main/java/com/notification/handler/NotificationHandler.java
@@ -5,10 +5,6 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.KafkaEvent;
 
-/**
- * [GREEN] FIX: This is the updated Lambda handler.
- * It now gets the logger and logs each incoming message.
- */
 public class NotificationHandler implements RequestHandler<KafkaEvent, Void> {
 
     @Override
@@ -17,17 +13,21 @@ public class NotificationHandler implements RequestHandler<KafkaEvent, Void> {
         // 1. Get the logger from the Lambda context
         LambdaLogger logger = context.getLogger();
 
+        //1.1 Null check for the events
+        if(kafkaEvent == null || kafkaEvent.getRecords() == null) {
+        	logger.log("Lambda invoked but no records received.");
+        	return null;
+        }
+        
         logger.log("Lambda function invoked. Received " + kafkaEvent.getRecords().size() + " records.");
 
         // 2. Loop through all topics and records in the event
         for (String topic : kafkaEvent.getRecords().keySet()) {
             for (KafkaEvent.KafkaEventRecord record : kafkaEvent.getRecords().get(topic)) {
                 
-                // 3. Log the message value!
-                // This is the line that makes our [RED] test pass.
+                // 3. Log the message value
                 String messageValue = record.getValue();
                 logger.log("Received message from topic " + topic + ": " + messageValue);
-
             }
         }
         return null;
