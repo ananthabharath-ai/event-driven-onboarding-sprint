f8aa4917f35c3b1b80f4ba9c90e35d7e41b07d40
ananthabharath-ai <anantha.bharath@acuverconsulting.com>
2025-12-26 18:01:47 +0530

    added the crossorigin in userapi

--- UserApiService/UserApiService/src/main/java/com/user/controllers/UserController.java
+++ UserApiService/UserApiService/src/main/java/com/user/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.user.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
 public class UserController {	
   
     private final UserService userService;
