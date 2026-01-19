8e14c1ce839774eb7441c317064e190696eb27a4
bharath <anantha.bharath@acuverconsulting.com>
2026-01-19 15:47:46 +0530

    added the frontend to display the contents of backened and aws

---
+++ aws Frontend/config.js
@@ -0,0 +1,5 @@
+// update this file  when you restart tasks of user api service and profile api service
+const API_CONFIG = {
+    USER_API: "http://13.23.45.67:8080",
+    PROFILE_API: "http://54.12.34.56:8081"
+};
